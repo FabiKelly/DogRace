586cb8e40308d8853ecec7914f492ea32c3cce3b
Fabiana Kelly Alves Ribeiro <85958392+FabiKelly@users.noreply.github.com>
2023-11-27 23:59:36 -0300

    Update avatar.js

--- js/avatar.js
+++ js/avatar.js
@@ -1,10 +1,11 @@
 function salvarAvatar() {
     const avatarSelecionado = document.querySelector('input[name="avatar"]:checked');
-
+    
     if (avatarSelecionado) {
-        const avatarValue = avatarSelecionado.value;
+        // a URL do gif do avatar selecionado
+        const avatarUrl = avatarSelecionado.nextElementSibling.src;
 
-        // Salvar a escolha do usuário usando cookies
-        document.cookie = `avatar=${avatarValue}; expires=Fri, 31 Dec 9999 23:59:59 GMT`;
+        // Atualiza a imagem do cachorro no jogo com o avatar selecionado
+        document.getElementById('dog').querySelector('img').src = avatarUrl;   
     }
 }
